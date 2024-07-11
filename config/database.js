b3bb428ea13310f040ffe5a4b02bd613f196fa74
NUSSUQ <t.nussuq@gmail.com>
2024-07-11 08:15:46 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,15 +2,21 @@ const mongoose = require("mongoose");
 const dbUrl = process.env.MONGO_URI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(dbUrl).then((data) => {
-            console.log(`Database connected with ${data.connection.host}`);
-        });
-    } catch (error) {
-        console.log(error);
-        console.log(error.message);
-        setTimeout(connectDB, 5000);
-    }
+  try {
+    await mongoose
+      .connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000,
+      })
+      .then((data) => {
+        console.log(`Database connected with ${data.connection.host}`);
+      });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    console.log("Retrying in 5 seconds...");
+    setTimeout(connectDB, 5000);
+  }
 };
 
 module.exports = connectDB;
