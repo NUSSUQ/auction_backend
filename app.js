b51bc2ee93ab73201057efda9c6b3083e616954d
NUSSUQ <t.nussuq@gmail.com>
2024-07-14 20:30:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ const createAdminUser = async () => {
 };
 
 // Start the server
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   
   console.log(`Server is running on http://localhost:${PORT}/`);
